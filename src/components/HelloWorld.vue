8d3d35dea20efe1d7b5335906f86acb0031add98
exa-yama <masahito-yamaguchi@exa-corp.co.jp>
2023-02-07 17:01:02 +0900

    UTF-8に変換

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      �v���W�F�N�g�̍쐬�ɐ������܂����I
+      プロジェクトの作成に成功しました！
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
